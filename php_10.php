3165b7b59bba331aba896c011a6c1451b0a0a315
ONO <onoto04@gmail.com>
2022-12-22 07:59:15 +0000

    課題10まで

---
+++ php_10.php
@@ -0,0 +1,13 @@
+<!DOCTYYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        
+        <title>10課題</title>
+    </head>
+    <body>
+        <h1>view bladeを用いてhtmlを表示する。</h1>
+    </body>
+</html>
